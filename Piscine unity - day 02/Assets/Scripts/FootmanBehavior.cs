03177091f94b6f059fc8c55b4200f11b1dcb7beb
Corbeau <12152540+CorbeauBlanc@users.noreply.github.com>
2019-01-09 19:31:02 +0100

    màj move

--- Piscine unity - day 02/Assets/Scripts/FootmanBehavior.cs
+++ Piscine unity - day 02/Assets/Scripts/FootmanBehavior.cs
@@ -20,17 +20,54 @@ public class FootmanBehavior : MonoBehaviour
 	public float minDistance;
 	public float speed; 
 
-	private CharacterMovement currentMove = CharacterMovement.STOP;
+	public CharacterMovement currentDirection = CharacterMovement.STOP;
 	private Vector2 newPosition;
 	private Vector2 currentVector;
 	private Rigidbody2D body;
 	private Camera mainCam;
 
+
 	public void MoveTo(Vector2 coords)
 	{
 		newPosition = coords;
 		currentVector = (coords - (Vector2)gameObject.transform.position).normalized;
-		currentMove = CharacterMovement.UP;
+		currentDirection = GetCurrentDirection(currentVector);
+		gameObject.GetComponent<Animator>().SetTrigger(GetCurrentTrigger(currentDirection));
+	}
+
+	private CharacterMovement GetCurrentDirection(Vector2 currentVector)
+	{
+		float angle = Vector2.SignedAngle(new Vector2(1, 0), currentVector);
+		if ((angle >= -22.5 && angle <= 0) || (angle >= 0 && angle <= 22.5))
+			return CharacterMovement.RIGHT;
+		if (angle >= 22.5 && angle <= 67.5)
+			return CharacterMovement.UPRIGHT;
+		if (angle >= 67.5 && angle <= 112.5)
+			return CharacterMovement.UP;
+		if (angle >= 112.5 && angle <= 157.5)
+			return CharacterMovement.UPLEFT;
+		if ((angle >= 157.5 && angle <= 180) || (angle <= -157.5 && angle >= -180))
+			return CharacterMovement.LEFT;
+		if (angle <= -112.5 && angle >= -157.5)
+			return CharacterMovement.DOWNLEFT;
+		if (angle <= -67.5 && angle >= -112.5)
+			return CharacterMovement.DOWN;
+		if (angle <= -22.5 && angle >= -67.5)
+			return CharacterMovement.DOWNRIGHT;
+		return CharacterMovement.STOP;
+	}
+
+	private string GetCurrentTrigger(CharacterMovement currentDirection)
+	{
+		return (new string[] { "Stop",
+							"Walk up",
+							"Walk up left",
+							"Walk left",
+							"Walk down left",
+							"Walk down",
+							"Walk down right",
+							"Walk right",
+							"Walk up right" })[(int)currentDirection];
 	}
 
     // Start is called before the first frame update
@@ -46,10 +83,13 @@ public class FootmanBehavior : MonoBehaviour
 		mainCam.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -10);
 		if (Input.GetButtonDown("Fire1"))
 			MoveTo(mainCam.ScreenToWorldPoint(Input.mousePosition));
-		if (currentMove != CharacterMovement.STOP)
+		if (currentDirection != CharacterMovement.STOP)
 		{
 			if (Vector2.Distance(gameObject.transform.position, newPosition) < minDistance)
-				currentMove = CharacterMovement.STOP;
+			{
+				gameObject.GetComponent<Animator>().SetTrigger("Stop");
+				currentDirection = CharacterMovement.STOP;
+			}
 			else
 				body.MovePosition((Vector2)gameObject.transform.position + (currentVector * speed * Time.deltaTime));
 		}
