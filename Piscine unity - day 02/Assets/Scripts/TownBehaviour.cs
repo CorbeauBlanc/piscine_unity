768572b4a4b744177fb8a2ec76f7937d7ddebd18
Corbeau <12152540+CorbeauBlanc@users.noreply.github.com>
2019-01-10 00:11:54 +0100

    update buildings

---
+++ Piscine unity - day 02/Assets/Scripts/TownBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownBehaviour : MonoBehaviour
+{
+	public GameObject spawnPoint;
+	public GameObject soldier;
+
+	private float spawnTime = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+		spawnTime -= Time.deltaTime;
+		if (spawnTime <= 0)
+		{
+			Instantiate(soldier, spawnPoint.transform.position, Quaternion.identity);
+			spawnTime = 10;
+		}
+    }
+}
