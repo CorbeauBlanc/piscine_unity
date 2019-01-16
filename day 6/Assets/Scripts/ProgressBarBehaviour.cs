767417d5e65b126d4c6c80c87a85847c9b883076
Corbeau <12152540+CorbeauBlanc@users.noreply.github.com>
2019-01-16 22:20:50 +0100

    finish

---
+++ day 6/Assets/Scripts/ProgressBarBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBarBehaviour : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+		if (transform.localScale.x > 1)
+		{
+			SoundManager.instance.playMusicPanic();
+			//SoundManager.instance.playAlarm();
+		}
+		else
+			SoundManager.instance.playMusicNormal();
+	}
+}
---
+++ day 6/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public static SoundManager instance { get; private set; }
+
+	public AudioSource normal;
+	public AudioSource panic;
+	public AudioSource alarm;
+
+	private void Start() => instance = this;
+
+	public void playMusicNormal()
+	{
+		if (!normal.isPlaying)
+		{
+			panic.Stop();
+			normal.Play();
+		}
+	}
+
+	public void playMusicPanic()
+	{
+		if (!panic.isPlaying)
+		{
+			normal.Stop();
+			panic.Play();
+		}
+	}
+
+	public void playAlarm()
+	{
+		if (!alarm.isPlaying)
+			alarm.Play();
+	}
+}
