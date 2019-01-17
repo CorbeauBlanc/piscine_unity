05415b96f8019f0c3c341f6b18659be37d788952
Corbeau <12152540+CorbeauBlanc@users.noreply.github.com>
2019-01-17 17:30:11 +0100

    update d7

---
+++ day 7/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+	public int speed;
+	public GameObject body;
+	public Rigidbody tankRigidBody;
+
+	private void FixedUpdate()
+	{
+		float axis;
+
+		if ((axis = Input.GetAxis("Vertical")) > 0 || axis < 0)
+			tankRigidBody.AddForce(-axis * speed * body.transform.up);
+		if ((axis = Input.GetAxis("Horizontal")) > 0 || axis < 0)
+			body.transform.localEulerAngles += Vector3.forward * axis;
+	}
+}
