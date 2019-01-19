be95db4f79950a2c67bb19c4408b6a5c72933bd3
Corbeau <12152540+CorbeauBlanc@users.noreply.github.com>
2019-01-19 16:30:41 +0100

    day9

---
+++ day 9/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+	public WeaponBehaviour[] weapons;
+	public int currentSelectedWeapon = 0;
+	public GameObject impactDust;
+	public GameObject impactShower;
+	public GameObject impactExplosion;
+
+	private float fwCurrentTime;
+
+	private void selectWeapon(int nb)
+	{
+		foreach(WeaponBehaviour weapon in weapons)
+			weapon.gameObject.SetActive(false);
+		weapons[nb].gameObject.SetActive(true);
+		currentSelectedWeapon = nb;
+	}
+
+	private void Update()
+	{
+		fwCurrentTime += Time.deltaTime;
+
+		if (Input.GetButton("Fire1") && fwCurrentTime > weapons[currentSelectedWeapon].weaponFireRate)
+		{
+			weapons[currentSelectedWeapon].shoot();
+			fwCurrentTime = 0;
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, weapons[currentSelectedWeapon].weaponFireRange, ~LayerMask.GetMask("Player")))
+			{
+				Instantiate(impactDust, hit.point, Quaternion.identity);
+				if (currentSelectedWeapon == 1)
+					Instantiate(impactExplosion, hit.point, Quaternion.identity);
+				else
+					Instantiate(impactShower, hit.point, Quaternion.identity);
+			}
+		}
+
+		if (Input.GetKeyDown("1"))
+			selectWeapon(0);
+		if (Input.GetKeyDown("2"))
+			selectWeapon(1);
+	}
+}
---
+++ day 9/Assets/Scripts/WeaponBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour
+{
+	public Animator weaponAnimator;
+	public AudioSource shootingAudio;
+	public ParticleSystem shootingParticle;
+	public float weaponFireRate;
+	public float weaponFireRange;
+
+	public void shoot()
+	{
+		shootingAudio.Play();
+		shootingParticle.Play();
+		weaponAnimator.SetTrigger("Shooting");
+	}
+}
