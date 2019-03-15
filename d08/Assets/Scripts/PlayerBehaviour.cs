8da5df8a7c9e562f9c94934aa1c51b966b122ce1
Corbeau <12152540+CorbeauBlanc@users.noreply.github.com>
2019-03-15 23:48:02 +0100

    update d8

---
+++ d08/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	public NavMeshAgent agent;
+	public Animator animator;
+	public Vector3 cameraOffset;
+
+	public int health = 100;
+
+	private void Die()
+	{
+		animator.SetTrigger("Die");
+	}
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	private void Update()
+	{
+		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, transform.position + cameraOffset, Time.deltaTime);
+		animator.SetBool("Is Running", agent.remainingDistance > 1);
+		if (Input.GetButtonUp("Fire1"))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, LayerMask.GetMask("Terrain")))
+				agent.destination = hit.point;
+		}
+	}
+
+
+}
