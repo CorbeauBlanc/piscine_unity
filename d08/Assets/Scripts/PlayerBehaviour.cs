b12af687784e300b5feb230859ca675f2a9a020d
Corbeau <12152540+CorbeauBlanc@users.noreply.github.com>
2019-03-16 21:51:25 +0100

    update d8

--- d08/Assets/Scripts/PlayerBehaviour.cs
+++ d08/Assets/Scripts/PlayerBehaviour.cs
@@ -9,11 +9,42 @@ public class PlayerBehaviour : MonoBehaviour {
 	public Animator animator;
 	public Vector3 cameraOffset;
 
-	public int health = 100;
+	private ZombieBehaviour target = null;
+	private int lastLoopNumber = -1;
+	private bool isAttacking = false;
 
-	private void Die()
+	private void Attack()
 	{
-		animator.SetTrigger("Die");
+		int currentLoopNumber;
+		if (!animator.GetCurrentAnimatorStateInfo(0).IsName("AttackMaya"))
+			return;
+		currentLoopNumber = (int)animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+		if (currentLoopNumber > lastLoopNumber)
+		{
+			lastLoopNumber = currentLoopNumber;
+			if (target.TakeDamage())
+				target = null;
+			isAttacking = false;
+		}
+	}
+
+	private void ProcessInputs()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, LayerMask.GetMask("Zombie", "Terrain")))
+			{
+				if (hit.collider.tag == "Ennemi")
+					target = hit.collider.GetComponentInParent<ZombieBehaviour>();
+				else
+					target = null;
+				agent.isStopped = false;
+				agent.destination = hit.point;
+			}
+		}
+		if (Input.GetButton("Fire1") && target)
+			isAttacking = true;
 	}
 
 	/// <summary>
@@ -22,13 +53,34 @@ public class PlayerBehaviour : MonoBehaviour {
 	private void Update()
 	{
 		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, transform.position + cameraOffset, Time.deltaTime);
-		animator.SetBool("Is Running", agent.remainingDistance > 1);
-		if (Input.GetButtonUp("Fire1"))
+
+		if (target)
 		{
-			RaycastHit hit;
-			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, LayerMask.GetMask("Terrain")))
-				agent.destination = hit.point;
+			agent.destination = target.transform.position;
+			if (agent.remainingDistance <= 1.5 && isAttacking)
+			{
+				animator.SetBool("Is Running", false);
+				animator.SetBool("Is Attacking", true);
+				agent.isStopped = true;
+				transform.LookAt(target.transform);
+				Attack();
+			}
+			else
+				animator.SetBool("Is Running", true);
 		}
+		else
+			animator.SetBool("Is Running", !agent.isStopped && agent.remainingDistance > 1);
+
+		ProcessInputs();
+
+		if (!isAttacking)
+		{
+			target = null;
+			animator.SetBool("Is Attacking", false);
+		}
+
+		if (!target)
+			lastLoopNumber = -1;
 	}
 
 
---
+++ d08/Assets/Scripts/ZombieBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieBehaviour : MonoBehaviour {
+	public int health = 3;
+
+	private PlayerBehaviour target = null;
+	private NavMeshAgent agent;
+	private Animator animator;
+
+	public bool TakeDamage()
+	{
+		health--;
+		if (health <= 0)
+		{
+			target = null;
+			animator.SetTrigger("Die");
+			agent.enabled = false;
+		}
+		return health <= 0;
+	}
+
+	private void Attack()
+	{
+		agent.isStopped = true;
+		animator.SetBool("Is Attacking", true);
+	}
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		animator.SetBool("Is Running", target != null && agent.remainingDistance > 1.5);
+
+		if (target)
+		{
+			agent.destination = target.transform.position;
+			if (agent.remainingDistance <= 1.5)
+				Attack();
+			else
+				agent.isStopped = false;
+		}
+	}
+
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			agent.destination = other.transform.position;
+			target = other.GetComponent<PlayerBehaviour>();
+		}
+	}
+}
